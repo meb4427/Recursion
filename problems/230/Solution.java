88a26f23fedc06dbb88650ecab52e0e52aa31ac8
Sadanori Maeba <sadanori.maeba@email.com>
2024-06-17 19:23:25 +0900

    230の回答の改良(初期条件の見直しによる，if文の削除)

--- problems/230/Solution.java
+++ problems/230/Solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public static String reduceByFive(int num) {
-        return reduceByFiveHelper(num, num, Integer.toString(num), false);
+        return reduceByFiveHelper(num, num - 5, Integer.toString(num), false);
     }
 
     public static String reduceByFiveHelper(int num, int currentNum, String res, boolean isUp) {
         if (currentNum == num && isUp) {
             return res + "," + Integer.toString(currentNum);
-        } else if (currentNum == num && !isUp) {
-            return reduceByFiveHelper(num, currentNum - 5, Integer.toString(currentNum), false);
         } else if (!isUp && currentNum >= 0) {
             return reduceByFiveHelper(num, currentNum - 5, res + ',' + Integer.toString(currentNum), false);
         } else if (currentNum < 0) {
@@ -18,10 +16,15 @@ class Solution {
     }
 
     public static void main(String[] args) {
+        System.out.println(Solution.reduceByFive(16));
         System.out.println(Solution.reduceByFive(16).equals("16,11,6,1,-4,1,6,11,16"));
+        System.out.println(Solution.reduceByFive(9));
         System.out.println(Solution.reduceByFive(9).equals("9,4,-1,4,9"));
+        System.out.println(Solution.reduceByFive(2));
         System.out.println(Solution.reduceByFive(2).equals("2,-3,2"));
+        System.out.println(Solution.reduceByFive(0));
         System.out.println(Solution.reduceByFive(0).equals("0,-5,0"));
+        System.out.println(Solution.reduceByFive(22));
         System.out.println(Solution.reduceByFive(22).equals("22,17,12,7,2,-3,2,7,12,17,22"));
     }
 }
