2c072a917ca6ef61db170b2ca2b3f6b2f55e6493
Sadanori Maeba <sadanori.maeba@email.com>
2024-06-17 22:12:27 +0900

    229再挑戦済

--- problems/229/Solution.java
+++ problems/229/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public static int kthElement(int n, int k) {
-        System.out.println("n: " + n + " k: " + k);
-        if (n == 1) return 0;
-
-        if (k % 2 == 0) {
-            if (kthElement(n - 1, k / 2) == 0) {
+        // System.out.println("n: " + n + " k: " + k);
+        if (n == 1) {
+            return 0;
+        } else if (k % 2 == 0) {
+            if (kthElement(n-1, k/2) == 0) {
                 return 1;
             } else {
                 return 0;
             }
         } else {
-            if (kthElement(n - 1, (k + 1) / 2) == 0){
+            if (kthElement(n-1, (k+1)/2) == 0) {
                 return 0;
             } else {
                 return 1;
