523f3aa86359f300ab4514feb9eeae5a6b12054b
Sadanori Maeba <sadanori.maeba@gmail.com>
2024-06-18 16:59:33 +0900

    240618win の作業保存

---
+++ 02midium03scope/020301scope/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static String firstName = "Masamune";
+    public static String lastName = "Watanabe";
+
+    public static String userName() {
+        String firstName = "Emily";
+        String lastName = "Robertson";
+
+        return firstName + "-" + lastName;
+    }
+}
